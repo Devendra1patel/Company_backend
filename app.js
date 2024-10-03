63dc6a17b03aef7926b1415f149d5febe3cb2779
devendra2002 <pateldevendra3131@gmail.com>
2024-10-03 16:47:46 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/services', require('./routes/services'));
+app.use('/api/portfolio-projects', require('./routes/portfolioProjects'));
+app.use('/api/social-media-links', require('./routes/socialMediaLinks'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
